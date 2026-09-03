5232364e00ca38d71a79de832d6cfa1354ce2f8a
Idris <kiadelakun@gmail.com>
2026-09-03 23:40:26 +0100

    Calculate probability of k heads in n tosses

---
+++ main.py
@@ -0,0 +1,26 @@
+import os 
+
+from lessons.counting_and_sets.counting import calculate_prob_k_heads as cpkh
+
+
+def main():
+
+    heads = 10
+    tosses = 100
+
+    print(cpkh(heads,tosses))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
