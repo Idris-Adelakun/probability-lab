2c0f7fb6b70ffc56345c5f3627ff1d0a61e29343
Idris <kiadelakun@gmail.com>
2026-09-06 22:43:20 +0100

    Get probability of a specific sum in a 2 die sample space

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os 
 
-from lessons.counting_and_sets.counting import calculate_prob_k_heads as cpkh
+import lessons.counting_and_sets.counting as lcs
+
+cpkh  = lcs.calculate_prob_k_heads
+tde = lcs.two_dice_experiment
 
 
 def main():
@@ -8,7 +10,25 @@ def main():
     heads = 10
     tosses = 100
 
-    print(cpkh(heads,tosses))
+    # print(cpkh(heads,tosses))
+
+    while True:
+        try:
+            target_sum = int(input("Please enter the sum of the dice:\n"))
+        except ValueError:
+            print("Please enter a whole number")
+            continue
+
+        if target_sum < 2 or target_sum > 12:
+            print("Your value must be greater than 2 and less than 12")
+            continue
+        break
+
+
+    probability = tde(target_sum)
+
+    print(f"The probability of getting {target_sum} is {probability:.3%}")
+
 
 
 
