90907528b7cb98222ff1fd9948da5c6be68785ac
Idris <kiadelakun@gmail.com>
2026-09-20 22:18:54 +0100

    Calculate E(x), Var(x), E(x^2)

--- lessons/discrete_random_variables.py
+++ lessons/discrete_random_variables.py
@@ -1,8 +1,22 @@
-import numpy as np
+import math
+from numpy import random as npr
 
-def disrecte_rv():
+def disrecte_rv(values, probabilities):
+    Ex = 0
+    Ex2 = 0
+    Var = 0
 
-    die1_face = np.random(1, 6, int)
-    die2_face = np.random(1, 6, int)
-
+    if len(values) != len(probabilities):
+        raise ValueError("List lengths do not match")
     
+
+    for value, probability in zip(values, probabilities):
+        Ex += value * probability
+        Ex2 += value**2 * probability
+
+    Var = Ex2 - Ex**2
+
+    return f"Ex: {Ex}\n Ex^2:{Ex2}\n Var(x):{Var}" 
+
+
+print(disrecte_rv([1, 2], [0.5, 0.5]))
